fc0a56861e5ba31d875c37651c72cbc9ef7b95b4
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-05-02 03:21:09 +0900

    add MultiByteWord

--- word_to_hex.cpp
+++ word_to_hex.cpp
@@ -17,6 +17,50 @@ char* ReadFile(FILE* fp) {
 	return buffer;
 }
 
+void AssertionWord(int pos) {
+	printf("なんかおかしい文字を使ってる．英数ひらカタ使え．\n");
+	printf("位置: %d byte\n", pos);
+	exit(-1);
+}
+
+char Small() {
+	
+}
+
+char MultiByteWord(char hi, char low) {
+	if (hi == 0x82 && 0x9f <= low && low <= 0xf1) {
+		/* ひらがな */
+	} else if (hi == 0x83 && 0x40 <= low && low <= 0x96) {
+		/* カタカナ */
+	} else if (hi == 0x89 && low == 0x7e) {
+		/* 円だけ例外 */
+		return 5f;
+	}
+
+	return -1;	// なんかおかしいときは-1を返す
+}
+
+void ProcParse(char* buffer) {
+	int i;
+	int cur = 0;
+	char* nametable = (char*)malloc(sizeof(buffer));
+	for (i = 0; i < sizeof(buffer); i++) {
+		if (0x20 <= buffer[i] && buffer[i] <= 0x7d) {
+			/* アルファベット処理 */
+			nametable[cur] -= 20;
+			cur++;
+		} else if (0x81 <= buffer[i] && buffer[i] <= 0x9f) {
+			/* 2バイト文字の処理 */
+			nametable[cur] = MultiByteWord(buffer[i], buffer[++i]);
+			if (nametable[cur] == -1)
+				AssertionWord(i);
+			cur++;
+		} else {
+			AssertionWord(i);
+		}
+	}
+}
+
 // メイン処理，　ファイルの読み込み等から
 void MainProc(char* argv) {
 	int i;
@@ -31,9 +75,7 @@ void MainProc(char* argv) {
 	fsize = FileSize(fp);
 	buffer = ReadFile(fp);
 	
-	for (i = 0; i < fsize; i++) {
-		printf("%c\n", buffer[i]);
-	}
+	ProcParse(buffer);
 
 	free(buffer);
 	fclose(fp);
@@ -47,6 +89,5 @@ int main(int argc, char* argv[]) {
 
 	MainProc(argv[1]);
 	
-
 	return 0;
 }
