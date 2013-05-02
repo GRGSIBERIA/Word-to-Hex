ab7c903b4cc6d63753963c1cd7c6715d456a7082
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-05-02 13:18:56 +0900

    implement AssignHiraKaku

--- word_to_hex.cpp
+++ word_to_hex.cpp
@@ -23,26 +23,75 @@ void AssertionWord(int pos) {
 	exit(-1);
 }
 
+#define LRANGE(X,Y) (X <= low && low <= Y)
+#define JA 		ntoffset + ((low % 2) == 1 ? 0x11 : 0x07)
+#define JKST	ntoffset + 0x16
+#define JN		ntoffset + 0x35
+#define JM		ntoffset + 0x3f
+#define JR		ntoffset + 0x47
+#define JH		ntoffset + 0x3A
+#define JWA		ntoffset + 0x4c
+#define JWO		ntoffset + 0x06
+#define JNN		ntoffset + 0x4d
+#define JY		ntoffset + ((low % 2) == 1 ? 0x44 : 0x0c)
+
+char AssignHiraKaku(char low, int hira=1) {
+	int ntoffset = (hira == 1 ? 0xc0 : 0x80);
+	int typesub = (hira == 1 ? 0x9f : 0x40);	/* ひらカタのテーブル最小値 */
+	low -= typesub;		/* 小文字のあからのオフセットに直す */
+
+	/* ひらがな */
+	if (LRANGE(0x00,0x08)) {
+		/* ひらがなの母音 */
+		return low / 2 + JA;
+	}
+	else if (LRANGE(0x0a,0x28)) {
+		/* かさた */
+		if (low == 0x22) 
+			return ntoffset + 0x0F;			/* 小文字のつは例外 */
+		if (0x22 <= low) low--;				/* 小文字のつ以降は自動で繰り下げ */
+		return low / 2 + JKST;
+	}
+	else if (LRANGE(0x29,0x2d)) {
+		return low - 0x29 + JN;				/* な */
+	}
+	else if (LRANGE(0x3d,0x42)) {
+		return low - 0x3d + JM;				/* ま */
+	}
+	else if (LRANGE(0x49,0x4d)) {
+		return low - 0x49 + JR;				/* ら */
+	}
+	else if (LRANGE(0x2e,0x3c)) {
+		return (low - 0x2e) / 3 + JH;	/* は */
+	}
+	else if (LRANGE(0x43,0x48)) {
+		return (low - 0x43) / 2 + JY; /* や */
+	}
+	else if (low == 0x4f) {
+		return JWA;	/* わ */
+	}
+	else if (low == 0x52) {
+		return JWO;	/* を */
+	}
+	else if (low == 0x53) {
+		return JNN;	/* ん */
+	}
+	return -1;
+}
+
+/* 濁点は全て無視されるので注意 */
 char MultiByteWord(char hi, char low) {
 	int pos = 0;
-	if (hi == 0x82 && 0x9f <= low && low <= 0xf1) {
-		/* ひらがな */
-		if (0x9f <= low && low <= 0xa8) {
-			/* ひらがなの母音 */
-			pos = (low - 0x9f) / 2;
-			return pos + ((low % 2) == 1 ? 0xC7 : 0xD1);
-		}
+	if (hi == 0x82 && LRANGE(0x9f,0xf1)) {
+		return AssignHiraKaku(low);
 
-	} else if (hi == 0x83 && 0x40 <= low && low <= 0x96) {
+	} else if (hi == 0x83 && LRANGE(0x40,0x96)) {
 		/* カタカナ */
-		if (0x40 <= low && low <= 0x49) {
-			/* かたかなの母音 */
-			pos = (low - 0x40) / 2;
-			return pos + ((low % 2) == 0 ? 0x87 : 0x91);
-		}
+		return AssignHiraKaku(low, 0);
+
 	} else if (hi == 0x89 && low == 0x7e) {
 		/* 円だけ例外 */
-		return 5f;
+		return 0x5f;
 	}
 
 	return -1;	// なんかおかしいときは-1を返す
